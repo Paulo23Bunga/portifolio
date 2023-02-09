6aab11d2ca06ceb9b3b1c95c4af67a30ad752a50
Rangel12 <Rangel12@CinfotecRangel.local>
2023-02-09 13:14:46 +0000

    commit Portfolio

---
+++ assets/js/custom.js
@@ -0,0 +1,149 @@
+(function($) {
+  var toggle = document.getElementById("menu-toggle");
+  var menu = document.getElementById("menu");
+  var close = document.getElementById("menu-close");
+
+  toggle.addEventListener("click", function(e) {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+    } else {
+      menu.classList.add("open");
+    }
+  });
+
+  close.addEventListener("click", function(e) {
+    menu.classList.remove("open");
+  });
+
+  // Close menu after click on smaller screens
+  $(window).on("resize", function() {
+    if ($(window).width() < 768) {
+      $(".main-menu a").on("click", function() {
+        menu.classList.remove("open");
+      });
+      
+    }
+  });
+
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    lazyLoad: true,
+    loop: true,
+    dots: true,
+    margin: 30,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+  $(".hover").mouseleave(function() {
+    $(this).removeClass("hover");
+  });
+
+  $(".isotope-wrapper").each(function() {
+    var $isotope = $(".isotope-box", this);
+    var $filterCheckboxes = $('input[type="radio"]', this);
+
+    var filter = function() {
+      var type = $filterCheckboxes.filter(":checked").data("type") || "*";
+      if (type !== "*") {
+        type = '[data-type="' + type + '"]';
+      }
+      $isotope.isotope({ filter: type });
+    };
+
+    $isotope.isotope({
+      itemSelector: ".isotope-item",
+      layoutMode: "masonry"
+    });
+
+    $(this).on("change", filter);
+    filter();
+  });
+
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true
+  });
+    
+      $("a[href='#section1']").click(()=>{
+          if ($(window).width() < 768) {
+      menu.classList.remove("open");
+        
+          }
+    })
+        $("a[href='#section2']").click(()=>{
+          if ($(window).width() < 768) {
+      menu.classList.remove("open");
+        
+          }
+    })
+        $("a[href='#section3']").click(()=>{
+          if ($(window).width() < 768) {
+      menu.classList.remove("open");
+        
+          }
+    })
+        $("a[href='#section4']").click(()=>{
+          if ($(window).width() < 768) {
+      menu.classList.remove("open");
+        
+          }
+    })
+    
+  $("#pic-mapa1").hide();
+  $("#pic-mapa2").hide();
+  $("#pic-mapa3").hide();
+
+  $("#pic-mapa").click(function(){
+  //  alert("Ola mundo");
+    $("#pic-mapa").hide();
+    $("#pic-mapa1").fadeIn('slow');
+  });
+
+  $("#pic-mapa1").click(function(){
+  //  alert("Ola mundo");
+    $("#pic-mapa1").hide();
+    $("#pic-mapa2").fadeIn('slow');
+  });
+
+  $("#pic-mapa2").click(function(){
+  //  alert("Ola mundo");
+    $("#pic-mapa2").hide();
+    $("#pic-mapa3").fadeIn('slow');
+  });
+
+    $("#txt-mapa").click(function(){
+  //  alert("Ola mundo");
+      $("#pic-mapa1").hide();
+  $("#pic-mapa2").hide();
+  $("#pic-mapa3").hide();
+    $("#pic-mapa").fadeIn('slow');
+  });
+
+    $(".btn-close").click(function(){
+  //  alert("Ola mundo");
+      $("#pic-mapa1").hide();
+  $("#pic-mapa2").hide();
+  $("#pic-mapa3").hide();
+    $("#pic-mapa").fadeIn('slow');
+  });
+
+    $(".fechar").click(function(){
+  //  alert("Ola mundo");
+      $("#pic-mapa1").hide();
+  $("#pic-mapa2").hide();
+  $("#pic-mapa3").hide();
+    $("#pic-mapa").fadeIn('slow');
+  });
+
+})(jQuery);
